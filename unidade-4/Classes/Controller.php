dd37af090426b9cd5468cb00a84b7adf27343773
eduardoazevedo3 <eduardoazevedo3@gmail.com>
2015-08-18 17:44:28 -0300

    Projeto de software desenvolvido na unidade 4

---
+++ unidade-4/Classes/Controller.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Controlador genérico
+ */
+
+namespace Classes;
+
+class Controller {
+    
+    // Parametros via url
+    protected $parametros;
+
+    // Método construtor da classe
+    public function __construct() {
+    }
+
+}
